cb87031f36dcbedaa6dbdceb6b6abb6b976fb2e4
prismic-levi <lga@zengularity.com>
2016-11-08 17:41:53 +0100

    Add onboarding validation

--- app/app.php
+++ app/app.php
@@ -19,6 +19,16 @@ use Prismic\Predicates;
 
 require_once 'includes/http.php';
 
+$apiEndpoint = $WPGLOBAL['app']->getContainer()->get('settings')['prismic.url'];
+$repoEndpoint = str_replace("/api", "", $apiEndpoint);
+$url = $repoEndpoint . '/app/settings/onboarding/run';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POST,1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, array());
+$result=curl_exec ($ch);
+curl_close ($ch);
+
 // Index page
 $app->get('/', function ($request, $response) use ($app, $prismic) {
   
